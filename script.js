27f2a251a044df9494d542edd548bdb927e7167e
Nathan <rensly2208@gmail.com>
2022-09-01 21:23:46 +0800

    finishing touches

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-let computer, results;
+let computer, results; //choice
+let compScore = 0, playScore = 0; //individual scores
 let button = document.querySelectorAll('.user');
 let enemyText = document.querySelector('.enemy');
 let text = document.querySelector('.result');
+let score = document.querySelector('.score');
 
 button.forEach(button => button.addEventListener('click', function(){
     switch(Number(this.value)){
         case 1:
             console.log(1);
             startGame('paper');
+            checkScore();
             break;
         case 2:
             console.log(2);
             startGame('scissors');
+            checkScore();
             break;
         default:
             startGame('rock');
+            checkScore();
             console.log(3);
     }
 }))
-
 
 function startGame(newInput){
     getComputerChoice();
     winDecider(newInput, computer);
     enemyText.textContent = `💻:The enemy's choice is ${computer}`;
-    text.textContent = results;
-    setTimeout(function(){
-        enemyText.textContent = "💻:The Enemy's thinking...";
-    }, 2000);
+    enemyText.classList.remove('animation');
+    text.innerHTML += `${results}<br>`;
+    score.textContent = `${compScore}-${playScore}`;
+}
+
+function checkScore(){
+    if(compScore == 5 || playScore == 5){
+        enemyText.textContent = "time out bruh😴"
+        enemyText.classList.add('lose-animation')
+        if(compScore == 5){
+            text.textContent = 'you lose';
+        }
+        else{
+            text.textContent = 'you win';
+        }
+        button.forEach(button => button.disabled = true)
+    }
 }
 
 function getComputerChoice() {
@@ -57,30 +74,36 @@ function winDecider(players, computers){
                     break;
                 case 'paper':
                     results = "lol take this L😹";
+                    ++compScore;
                     break;
                 case 'scissors':
                     results = "damn, W king🥶";
+                    ++playScore;
             }
             break;
         case 'paper':
             switch (computers){
                 case 'rock':
                     results = "damn, W king🥶";
+                    ++playScore;
                     break;
                 case 'paper':
                     results = "it's a tie🥱";
                     break;
                 case 'scissors':
                     results = "lol take this L😹";
+                    ++compScore;
             }
             break;
         case 'scissors':
             switch (computers){
                 case 'rock':
                     results = "lol take this L😹";
+                    ++compScore;
                     break;
                 case 'paper':
                     results = "damn, W king🥶";
+                    ++playScore;
                     break;
                 case 'scissors':
                     results = "it's a tie🥱";
